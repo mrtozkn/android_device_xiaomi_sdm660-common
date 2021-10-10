a3cc80444586221bfce2dfd204b01681b18248e9
Danny Lin <danny@kdrag0n.dev>
2021-10-10 02:58:52 -0700

    sdm660-common: Switch to Vulkan UI renderer
    We tried doing this on Android 11 and later reverted it due to fd leaks
    during prolonged video calls. However, the issue has been fixed on
    Android 12, so let's enable it again for performance.
    
    Change-Id: I1cd2ca6931aad60f9c48d40aa0212b69c68edef8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,6 +59,7 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
+TARGET_USES_VULKAN := true
 
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
