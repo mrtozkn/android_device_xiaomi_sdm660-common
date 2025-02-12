0fe4d1b3f097268b8ee8bdc2d9086143e9b5e9e9
Mert Ozkan <mertyozkan@trakya.edu.tr>
2025-02-12 20:02:46 +0300

    sdm660-common: remove copy of libutits-v33

--- sdm660.mk
+++ sdm660.mk
@@ -393,7 +393,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+    libutils-v33
 
 # Wifi
 PRODUCT_PACKAGES += \
